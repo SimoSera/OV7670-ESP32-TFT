209014470c380afd52018454c2cc34d44e0a8452
Simone Serafini <63721643+SimoSera@users.noreply.github.com>
2024-05-05 00:50:28 +0200

    Update OV7670_ESP32_TFT.ino
    Added my edits

--- OV7670_ESP32_TFT/OV7670_ESP32_TFT.ino
+++ OV7670_ESP32_TFT/OV7670_ESP32_TFT.ino
@@ -1,5 +1,13 @@
 // Non-FIFO OV7670 Video on TFT with ESP32
 // based on https://github.com/kobatan/OV7670-ESP32
+/*
+ * 
+ * (not tested)
+ * could fit an sd card reader on spi pins:
+ * 12 13 14 15
+ * remember that pin 12 is dangeroud: boot fails if pulled high, strapping pin
+ * other pins that are free: 0 1 2 3 (0 1 3 are special pins that should not be used and 2 is onboard led)
+ */
 
 #define MODE QVGA  // 320 X 240
 //#define MODE      QQVGA  // 160 x 120
@@ -15,14 +23,14 @@
 #include <SPI.h>
 #include <OV7670.h>
 #include <Adafruit_GFX.h>
-#include <Adafruit_ILI9341.h>
+#include <Adafruit_ST7789.h>
 
 // OV7670 pins
 // SSCB_SDA(SIOD) -> 21(ESP32)
 // SSCB_SCL(SIOC) -> 22(ESP32)
 // RESET          -> 3.3V
-// PWDN		        -> GND
-// HREF		        -> NC
+// PWDN            -> GND
+// HREF           -> NC
 const camera_config_t cam_conf = {
   .D0 = 36,
   .D1 = 39,
@@ -33,24 +41,34 @@ const camera_config_t cam_conf = {
   .D6 = 25,
   .D7 = 26,
   .XCLK = 27,
-  .PCLK = 14,
-  .VSYNC = 13,
+  .PCLK = 19,  //14
+  .VSYNC = 4,  //13
   .xclk_freq_hz = 10000000,  // XCLK 10MHz
   .ledc_timer = LEDC_TIMER_0,
   .ledc_channel = LEDC_CHANNEL_0
 };
 
 // TFT pins
-#define TFT_DC 16
-#define TFT_CS 5
-#define TFT_RST 17
-#define TFT_MISO 19
-#define TFT_MOSI 23
-#define TFT_CLK 18
+#define TFT_CS         5
+#define TFT_RST        16 // Or set to -1 and connect to Arduino RESET pin
+#define TFT_DC         17
 
-Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_RST);
+
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
 OV7670 cam;
-uint16_t *buf, w, h;
+uint16_t *buf1,*buf2, w, h;
+//MultiThreading
+TaskHandle_t Task1;
+void Task1code(void * pvParameters){
+  for(;;){
+    for (uint16_t y = 0; y < h; y+=2) {
+      buf1 = cam.getLine(y + 1);
+      tft.drawRGBBitmap(0, y, buf1, w, 1);
+    }
+  }
+}
+
+
 
 void setup() {
   setCpuFrequencyMhz(240);
@@ -59,10 +77,9 @@ void setup() {
   
   Wire.begin();
   Wire.setClock(400000);
-
-  tft.begin();
+  tft.init(240, 320);
   tft.setRotation(ROTATION);
-  tft.fillScreen(0);
+  tft.fillScreen(ST77XX_BLACK);
 
   esp_err_t err = cam.init(&cam_conf, MODE, COLOR);
   if (err != ESP_OK) Serial.println("cam.init ERROR");
@@ -89,12 +106,19 @@ void setup() {
       w = 88;
       h = 72;
   }
+  xTaskCreatePinnedToCore(
+      Task1code, /* Function to implement the task */
+      "Task1", /* Name of the task */
+      10000,  /* Stack size in words */
+      NULL,  /* Task input parameter */
+      0,  /* Priority of the task */
+      &Task1,  /* Task handle. */
+      0); /* Core where the task should run */
 }
 
 void loop(void) {
-
-  for (uint16_t y = 0; y < h; y++) {
-    buf = cam.getLine(y + 1);
-    tft.drawRGBBitmap(0, y, buf, w, 1);
-  }
+  for (uint16_t y = 1; y < h; y+=2) {
+      buf2 = cam.getLine(y + 1);
+      tft.drawRGBBitmap(0, y, buf2, w, 1);
+    }
 }
